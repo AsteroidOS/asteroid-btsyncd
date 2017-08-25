7dc4c6e81487c31e63b25b8da6035559a6a42c98
Inoyatullokhon Junaydullaev <mronx@ya.ru>
2017-08-25 09:19:09 +0000

    Added translation using Weblate (Uzbek (latin))

---
+++ i18n/asteroid-btsyncd.uz_Latn.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="uz_Latn">
+<context>
+    <name>BlueZManager</name>
+    <message>
+        <location filename="../bluezmanager.cpp" line="157"/>
+        <source>Connected</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../bluezmanager.cpp" line="161"/>
+        <source>Disconnected</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
