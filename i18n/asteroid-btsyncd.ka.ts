e2c38d0754e6b7d9bc99513dcb5d74bca348a663
sam bowlby <sam@chven.us>
2017-11-11 14:29:24 +0000

    Added translation using Weblate (Georgian)

---
+++ i18n/asteroid-btsyncd.ka.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ka">
+<context>
+    <name>BlueZManager</name>
+    <message>
+        <location filename="../bluezmanager.cpp" line="157"/>
+        <source>Connected</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../bluezmanager.cpp" line="161"/>
+        <source>Disconnected</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
