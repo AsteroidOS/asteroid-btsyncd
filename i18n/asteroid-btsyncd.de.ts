1c295fb5dca0323fffdaea8f13bc09bc1c9398b1
DNS <dns@88it.de>
2018-03-18 11:24:29 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: AsteroidOS/Bluetooth Sync Daemon
    Translate-URL: https://hosted.weblate.org/projects/asteroidos/btsyncd/de/

--- i18n/asteroid-btsyncd.de.ts
+++ i18n/asteroid-btsyncd.de.ts
@@ -6,12 +6,12 @@
     <message id="id-connected">
         <location filename="../bluezmanager.cpp" line="158"/>
         <source>Connected</source>
-        <translation></translation>
+        <translation>Verbunden</translation>
     </message>
     <message id="id-disconnected">
         <location filename="../bluezmanager.cpp" line="163"/>
         <source>Disconnected</source>
-        <translation></translation>
+        <translation>Getrennt</translation>
     </message>
 </context>
 </TS>
