a6fc1932d55d9c07963c595ac30418964e75bf13
Juraj Perenyi <gyperenyi@yahoo.com>
2018-04-23 17:55:50 +0000

    Added translation using Weblate (Hungarian)

---
+++ i18n/asteroid-btsyncd.hu.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hu">
+<context>
+    <name></name>
+    <message id="id-connected">
+        <location filename="../bluezmanager.cpp" line="158"/>
+        <source>Connected</source>
+        <translation></translation>
+    </message>
+    <message id="id-disconnected">
+        <location filename="../bluezmanager.cpp" line="163"/>
+        <source>Disconnected</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
