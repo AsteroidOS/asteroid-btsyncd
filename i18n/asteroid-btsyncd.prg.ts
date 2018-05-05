e6ec440726bacc1ca800ef46bd94872d8c38a23a
Vitalis Fischer <vitalis.fischer@web.de>
2018-05-05 14:41:12 +0000

    Translated using Weblate (Prussian)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: AsteroidOS/Bluetooth Sync Daemon
    Translate-URL: https://hosted.weblate.org/projects/asteroidos/btsyncd/prg/

--- i18n/asteroid-btsyncd.prg.ts
+++ i18n/asteroid-btsyncd.prg.ts
@@ -6,12 +6,12 @@
     <message id="id-connected">
         <location filename="../bluezmanager.cpp" line="158"/>
         <source>Connected</source>
-        <translation></translation>
+        <translation>соединен</translation>
     </message>
     <message id="id-disconnected">
         <location filename="../bluezmanager.cpp" line="163"/>
         <source>Disconnected</source>
-        <translation></translation>
+        <translation>отсоединен</translation>
     </message>
 </context>
 </TS>
