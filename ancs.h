b96c36563648ef7770f2c8b6a8cf539aec69ac54
Zbigniew Halas <zhalas@gmail.com>
2018-08-19 21:06:51 +0200

    ANCS decoding

--- ancs.h
+++ ancs.h
@@ -2,14 +2,28 @@
 #define ANCS_H
 
 #include <QObject>
+#include <QString>
+#include <QMap>
+#include <QVariant>
 
 class ANCS: public QObject
 {
     Q_OBJECT
 public slots:
     void SearchForAncsCharacteristics();
+private slots:
+    void NotificationCharacteristicPropertiesChanged( QString interfaceName,
+                                                      QMap<QString, QVariant> changedProperties, QStringList invalidatedProperties);
+    void DataCharacteristicPropertiesChanged( QString interfaceName,
+                                              QMap<QString, QVariant> changedProperties, QStringList invalidatedProperties);
 private:
+    QString controlCharacteristic;
     bool isMatchingCharacteristic(QString uuid, QMap<QString, QVariantMap> dbusObject);
+    void appendByte(QByteArray &arr, unsigned int val);
+    void append2Bytes(QByteArray &arr, unsigned int val);
+    unsigned int decodeNumber(const QByteArray &arr, int offset, int length);
+    int decodeStringAttribute(QByteArray bytes, int offset, int maxLenght, QString &result);
+    void prepareQuery(QByteArray &result, const QByteArray &msgid);
 };
 
 #endif // ANCS_H
