9d822729e5eea3c0f7fe25f99e7d5458fc88655f
Jörgen Sjöbom <josjo67@gmail.com>
2020-05-27 23:24:47 +0000

    Translated using Weblate (Swedish)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: AsteroidOS/Bluetooth Sync Daemon
    Translate-URL: https://hosted.weblate.org/projects/asteroidos/btsyncd/sv/

--- i18n/asteroid-btsyncd.sv.ts
+++ i18n/asteroid-btsyncd.sv.ts
@@ -6,12 +6,12 @@
     <message id="id-connected">
         <location filename="../bluezmanager.cpp" line="158"/>
         <source>Connected</source>
-        <translation type="unfinished"></translation>
+        <translation>Ansluten</translation>
     </message>
     <message id="id-disconnected">
         <location filename="../bluezmanager.cpp" line="163"/>
         <source>Disconnected</source>
-        <translation type="unfinished"></translation>
+        <translation>Frånkopplad</translation>
     </message>
 </context>
 </TS>
