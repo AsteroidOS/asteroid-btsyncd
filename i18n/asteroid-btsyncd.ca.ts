397021c032224cbb9f68376530fec240a0da69f6
Adolfo Jayme Barrientos <fitojb@ubuntu.com>
2021-02-13 01:46:34 +0100

    Added translation using Weblate (Catalan)

---
+++ i18n/asteroid-btsyncd.ca.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ca">
+<context>
+    <name></name>
+    <message id="id-connected">
+        <location filename="../bluezmanager.cpp" line="158"/>
+        <source>Connected</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-disconnected">
+        <location filename="../bluezmanager.cpp" line="163"/>
+        <source>Disconnected</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
