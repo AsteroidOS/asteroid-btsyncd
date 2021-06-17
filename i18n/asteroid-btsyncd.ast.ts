7d7637bb21b3445a98d484b33b088e25f313c44a
Enol P <enolp@softastur.org>
2021-06-17 01:33:50 +0000

    Translated using Weblate (Asturian)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: AsteroidOS/Bluetooth Sync Daemon
    Translate-URL: https://hosted.weblate.org/projects/asteroidos/btsyncd/ast/

--- i18n/asteroid-btsyncd.ast.ts
+++ i18n/asteroid-btsyncd.ast.ts
@@ -6,12 +6,12 @@
     <message id="id-connected">
         <location filename="../bluezmanager.cpp" line="158"/>
         <source>Connected</source>
-        <translation>Coneutóse</translation>
+        <translation>Conectóse</translation>
     </message>
     <message id="id-disconnected">
         <location filename="../bluezmanager.cpp" line="163"/>
         <source>Disconnected</source>
-        <translation>Desconeutóse</translation>
+        <translation>Desconectóse</translation>
     </message>
 </context>
 </TS>
