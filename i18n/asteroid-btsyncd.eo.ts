92b06a14328f9eb9531e2240a871fc40f5ddcce7
phlostically <phlostically@mailinator.com>
2022-05-18 09:25:41 +0000

    Translated using Weblate (Esperanto)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: AsteroidOS/Bluetooth Sync Daemon
    Translate-URL: https://hosted.weblate.org/projects/asteroidos/btsyncd/eo/

--- i18n/asteroid-btsyncd.eo.ts
+++ i18n/asteroid-btsyncd.eo.ts
@@ -6,12 +6,12 @@
     <message id="id-connected">
         <location filename="../src/bluezmanager.cpp" line="176"/>
         <source>Connected</source>
-        <translation type="unfinished"></translation>
+        <translation>Konektita</translation>
     </message>
     <message id="id-disconnected">
         <location filename="../src/bluezmanager.cpp" line="182"/>
         <source>Disconnected</source>
-        <translation type="unfinished"></translation>
+        <translation>Malkonektita</translation>
     </message>
 </context>
 </TS>
