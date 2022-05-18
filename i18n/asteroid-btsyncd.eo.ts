36a03082cd9f09f77e79060e02f6b4b44702aa53
phlostically <phlostically@mailinator.com>
2022-05-18 11:25:31 +0200

    Added translation using Weblate (Esperanto)

---
+++ i18n/asteroid-btsyncd.eo.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="eo">
+<context>
+    <name></name>
+    <message id="id-connected">
+        <location filename="../src/bluezmanager.cpp" line="176"/>
+        <source>Connected</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-disconnected">
+        <location filename="../src/bluezmanager.cpp" line="182"/>
+        <source>Disconnected</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
