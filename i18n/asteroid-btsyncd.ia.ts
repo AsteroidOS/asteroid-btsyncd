20f7976a5e58156219fbae05256525443d8c6e1f
Software In Interlingua <softinterlingua@gmail.com>
2024-09-25 03:11:54 +0000

    Translated using Weblate (Interlingua)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: AsteroidOS/Bluetooth Sync Daemon
    Translate-URL: https://hosted.weblate.org/projects/asteroidos/btsyncd/ia/

--- i18n/asteroid-btsyncd.ia.ts
+++ i18n/asteroid-btsyncd.ia.ts
@@ -11,7 +11,7 @@
     <message id="id-disconnected">
         <location filename="../src/bluezmanager.cpp" line="182"/>
         <source>Disconnected</source>
-        <translation type="unfinished"></translation>
+        <translation>Disconnexe</translation>
     </message>
 </context>
 </TS>
