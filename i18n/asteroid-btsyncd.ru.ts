e589c92bdcf1b2a5aec192482a2c4f6b64abdee7
Abduloh Raupov <abdulohraupov@gmail.com>
2025-07-31 23:52:28 +0200

    Translated using Weblate (Russian)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: AsteroidOS/Bluetooth Sync Daemon
    Translate-URL: https://hosted.weblate.org/projects/asteroidos/btsyncd/ru/

--- i18n/asteroid-btsyncd.ru.ts
+++ i18n/asteroid-btsyncd.ru.ts
@@ -6,7 +6,7 @@
     <message id="id-connected">
         <location filename="../src/bluezmanager.cpp" line="176"/>
         <source>Connected</source>
-        <translation>Соединен</translation>
+        <translation>Соединён</translation>
     </message>
     <message id="id-disconnected">
         <location filename="../src/bluezmanager.cpp" line="182"/>
