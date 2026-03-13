a8bab9af5547b92541bbc6635c1e02f165540814
Ed Beroset <beroset@ieee.org>
2026-03-13 16:56:05 -0400

    Fix copyright info for settime.* files
    Signed-off-by: Ed Beroset <beroset@ieee.org>

--- src/settime.h
+++ src/settime.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2026 - Florent Revest <revestflo@gmail.com>
+ * Copyright (C) 2026 - Ed Beroset <beroset@ieee.org>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
